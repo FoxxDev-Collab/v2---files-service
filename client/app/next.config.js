2195046ea247c438e7cf3196780688d5dcd69c0d
FoxxAdmin <admin@foxxcyber.com>
2024-09-12 08:58:41 -0600

    dependencies updated to node 20.17

--- client/app/next.config.js
+++ client/app/next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['localhost'], // Add your API domain here
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
