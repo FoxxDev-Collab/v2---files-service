0664050550edbe513aeb1f5ff829c9f55128a095
FoxxAdmin <admin@foxxcyber.com>
2024-09-12 14:30:52 -0600

    nextui packages

--- client/app/tailwind.config.ts
+++ client/app/tailwind.config.ts
@@ -1,3 +1,4 @@
+import {nextui} from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,11 +6,12 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(card|ripple).js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
   darkMode: 'class',
 };
 
